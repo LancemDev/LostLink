0aba17264023483b412d0e9820b7f80746662f45
LancemDev <munyaolance1@gmail.com>
2024-11-08 23:06:33 +0300

    :art: Feat: Authentication - Singup and Login using firebase auth

---
+++ app/src/main/java/com/example/firebaseauthdemoapp/pages/HomePage.kt
@@ -0,0 +1,45 @@
+package com.example.firebaseauthdemoapp.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.example.firebaseauthdemoapp.AuthState
+import com.example.firebaseauthdemoapp.AuthViewModel
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomePage(modifier: Modifier, navController: NavController, authViewModel: AuthViewModel){
+
+    val authState = authViewModel.authState.observeAsState()
+
+    LaunchedEffect(authState.value) {
+        when(authState.value){
+            is AuthState.Unauthenticated -> navController.navigate("login")
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Page", fontSize = 32.sp)
+
+        TextButton(onClick = {
+            authViewModel.signout()
+        }) {
+            Text(text = "Sign out")
+        }
+    }
+}
